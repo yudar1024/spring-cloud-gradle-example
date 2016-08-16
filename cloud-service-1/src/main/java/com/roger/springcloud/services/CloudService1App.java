55f5b61d2ccd6ec3b517ad198827d8ab0b9efa3d
roger <chen.rojer@gmail.com>
2016-08-16 12:08:07 +0800

    add spring cloud bus for config refresh

--- cloud-service-1/src/main/java/com/roger/springcloud/services/CloudService1App.java
+++ cloud-service-1/src/main/java/com/roger/springcloud/services/CloudService1App.java
@@ -2,6 +2,7 @@ package com.roger.springcloud.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringCloudApplication
 @RestController
+
 //启用hystrix stream ，可以在此服务的url(localhost:9101)后加/hystrix.stream 才看stream 流的数据，完整URL=localhost:9101/hystrix.stream.此stream 只搜集被@HystrixCommand 注解标注的方法的调用情况
 @EnableHystrix
 public class CloudService1App {
--- cloud-service-1/src/main/java/com/roger/springcloud/services/ServiceController.java
+++ cloud-service-1/src/main/java/com/roger/springcloud/services/ServiceController.java
@@ -2,6 +2,7 @@ package com.roger.springcloud.services;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by chenluo on 2016/8/14.
  */
 @RestController
+//启用远程配置刷新功能
+@RefreshScope
 public class ServiceController {
 
     @Value("${jdbc.url:default}")
